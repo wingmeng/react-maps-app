0ca48de9f08f42176f675ada76aebea2cbd17b77
wingmeng <wingmeng@qq.com>
2019-03-14 23:54:52 +0800

    :zap: 优化功能

--- src/iconBtn.js
+++ src/iconBtn.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import $script from 'scriptjs'
+import './iconBtn.scss'
 
 // 加载 svg 图标
 $script('//at.alicdn.com/t/font_1084739_lvg4ix9y5nm.js');
 
 export default (props) => {
   return (
-    <a href="javascript:;" className={props.className} role="button" title={props.title}
+    <button className={`icon-button ${props.className}`} title={props.title}
         onClick={() => props.handle()}>
       <svg className="icon" aria-hidden="true">
         <use href={`#${props.icon}`}></use>
       </svg>
-    </a>
+    </button>
   )
 }
