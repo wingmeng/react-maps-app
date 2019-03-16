8ba88cff5f5e750b7cb01f869e4580650d9ea809
wingmeng <wingmeng@qq.com>
2019-03-16 16:56:10 +0800

    :bug: 边界情况处理

--- src/iconBtn.js
+++ src/iconBtn.js
@@ -3,15 +3,35 @@ import $script from 'scriptjs'
 import './iconBtn.scss'
 
 // 加载 svg 图标
-$script('//at.alicdn.com/t/font_1084739_lvg4ix9y5nm.js');
+$script('//at.alicdn.com/t/font_1084739_y0tftj5rsk.js');
+
+/**
+ * Icon names:
+    icon-search
+    icon-hotel
+    icon-left
+    icon-close
+    icon-menu
+    icon-back
+    icon-cog
+ */
 
 export default (props) => {
   return (
-    <button className={`icon-button ${props.className}`} title={props.title}
-        onClick={() => props.handle()}>
-      <svg className="icon" aria-hidden="true">
-        <use href={`#${props.icon}`}></use>
-      </svg>
+    <button className={`icon-button ${props.className || ''}`}
+      type={props.type || ''}
+      title={props.title || ''}
+      disabled={props.disabled}
+      onClick={() => props.handle ? props.handle() : false}
+    >
+      {props.icon && (
+        <svg className="icon" aria-hidden="true">
+          <use href={`#${props.icon}`}></use>
+        </svg>
+      )}
+      {props.children && (
+        <span>{props.children}</span>
+      )}
     </button>
   )
 }
