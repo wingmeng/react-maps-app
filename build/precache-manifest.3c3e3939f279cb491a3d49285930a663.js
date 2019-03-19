ecacdf1caf58752e4686bfa3630524eb407ad5f6
wingmeng <wingmeng@qq.com>
2019-03-19 15:33:22 +0800

    :bookmark: 发行测试

---
+++ build/precache-manifest.3c3e3939f279cb491a3d49285930a663.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "14d5cea55d4cd5a2ff0ed1f1c11c58af",
+    "url": "/static/media/warning.14d5cea5.svg"
+  },
+  {
+    "revision": "cb3027510a6887b741f13fc43c387cd8",
+    "url": "/static/media/loading.cb302751.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6ccd76b4053612e63329",
+    "url": "/static/js/main.ac3d1190.chunk.js"
+  },
+  {
+    "revision": "07d00a09f402afd921fb",
+    "url": "/static/js/2.e55806c0.chunk.js"
+  },
+  {
+    "revision": "6ccd76b4053612e63329",
+    "url": "/static/css/main.74a46a12.chunk.css"
+  },
+  {
+    "revision": "7794f0abfc29ab8209003f374967eb21",
+    "url": "/index.html"
+  }
+];
