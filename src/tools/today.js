b13b7592b7fa4a479c7e88635cac78498daaf382
wingmeng <wingmeng@qq.com>
2019-03-19 15:41:27 +0800

    移除无用文件

--- src/tools/today.js
+++
@@ -1,8 +0,0 @@
-// 将当前日期对象转换为 yyyyMMdd 格式的日期字符串
-export default (today=new Date()) => {
-  const fixZero = num => num < 10 ? '0' + num : num;
-
-  return today.getFullYear() +
-    fixZero(today.getMonth() + 1) +
-    fixZero(today.getDate())
-}
